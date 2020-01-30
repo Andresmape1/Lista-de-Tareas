27d4cb13814a8832a4016152e0727fedcde41abc
Andres Maita <andres_mape1@hotmail.com>
2020-01-30 10:00:37 -0400

    version 1

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html land="end">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-scale-1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>lista de tareas</title>
+
+<!-- Bootstrap 4 -->	
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<!--FONT AWESOME 5 -->
+<script src="https://kit.fontawesome.com/1ccbf044e2.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+	<nav class="navbar navbar-dark bg-dark">
+		<div class= container>
+			<a href="index.php" class="navbar-brand"> LISTA DE TAREAS </a>
+		</div>
+		 
+	</nav>
